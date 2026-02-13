3b147d65051aa1f3f6aa7de266f2c03ebe629770
Levy-Y <65444028+Levy-Y@users.noreply.github.com>
2026-02-13 15:25:40 +0100

    Refactor StreamHandler to support KVS WebRTC streaming

--- src/core/stream_handler.py
+++ src/core/stream_handler.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python
 import asyncio
-from typing import Optional, Tuple
+import time
+from typing import Optional, Tuple, Any, List, Callable
 
 import gi
 import loguru
 import numpy as np
-import time
-
-from mypy.types import AnyType
 from ultralytics import YOLO
 
+from src.core.kinesis_video_manager import KinesisVideoClient
 from src.core.mqtt_manager import MqttManager
-from src.models.credentials_model import CredentialsModel
+from src.core.credential_provider import CredentialProvider
+from src.utils.gst_video_track import GstVideoTrack
 
 gi.require_version("Gst", "1.0")
 from gi.repository import Gst
@@ -23,10 +23,13 @@ class StreamHandler:
         port: int,
         yolo_path: str,
         sample_rate: int,
-        mqtt,
+        mqtt: MqttManager,
         alert_topic: str,
         presence_confirmation_frames: int,
         confidence_threshold: int,
+        channel_arn: str,
+        kvs_client_factory: Callable[..., KinesisVideoClient],
+        credential_provider: CredentialProvider,
     ) -> None:
         Gst.init(None)
 
@@ -37,22 +40,32 @@ class StreamHandler:
         self.alert_topic = alert_topic
         self.presence_confirmation_frames = presence_confirmation_frames
         self.confidence_threshold: float = confidence_threshold / 100
+        self.channel_arn = channel_arn
+        self.kvs_client_factory = kvs_client_factory
+        self.credential_provider = credential_provider
+
+        try:
+            self.aws_region = self.channel_arn.split(":")[3]
+        except IndexError:
+            self.aws_region = "eu-west-1"
+            loguru.logger.warning(
+                f"Could not parse region from ARN, defaulting to {self.aws_region}"
+            )
 
         self._running = False
         self._task = None
+        self._webrtc_task = None
+        self._frame = None
+        self._gst_track = None
+        self._kvs_client = None
+        self._video_pipe = None
+        self._video_sink = None
+        self._handler = None
 
         self._frame_count = 0
         self._last_process_time = 0
         self._consecutive_detection_frames = 0
         self._min_interval = 0.2
-
-        self._frame: Optional[np.ndarray] | None = None
-        self._video_pipe = None
-        self._video_sink = None
-        self._handler: Optional[int] | None = None
-
-        self._valve = None
-        self._ws_sink = None
 
     async def start(self):
         if self._running:
@@ -60,84 +73,113 @@ class StreamHandler:
 
         self._running = True
 
+        self._gst_track = GstVideoTrack()
+
+        self._kvs_client = None
+
         command = (
             f"udpsrc port={self.port} ! application/x-rtp, payload=96 ! rtph264depay ! h264parse ! "
-            "tee name=t "
-
-            # Branch 1: YOLO
-            "t. ! queue ! avdec_h264 ! videoconvert ! video/x-raw,format=BGR ! "
+            "avdec_h264 ! videoconvert ! video/x-raw,format=BGR ! "
             "appsink name=appsink emit-signals=true sync=false max-buffers=2 drop=true "
-
-            # Branch 2: AWS Kinesis WebRTC
-            "t. ! queue ! valve name=stream_valve drop=True ! "
-            "x264enc bitrate=512 tune=zerolatency speed-preset=ultrafast ! "
-            "video/x-h264,profile=baseline ! "
-            "awskvswebrtcsink name=ws aws-region=eu-west-1 "
         )
 
+        loguru.logger.info(f"Launching Pipeline: {command}")
         self._video_pipe = Gst.parse_launch(command)
         self._video_pipe.set_state(Gst.State.PLAYING)
+
         self._video_sink = self._video_pipe.get_by_name("appsink")
-
-        self._valve = self._video_pipe.get_by_name("stream_valve")
-        self._ws_sink = self._video_pipe.get_by_name("ws")
-
         self._handler = self._video_sink.connect("new-sample", self._decode_frame)
+
         self._task = asyncio.create_task(self._start_detection())
 
-    def set_streaming_enabled(self, enabled: bool):
-        if self._valve:
-            self._valve.set_property("drop", not enabled)
+    async def set_streaming_state(self, enabled: bool):
+        loguru.logger.info(f"Streaming enabled set to {enabled}")
 
-    def update_aws_credentials(self, creds: CredentialsModel):
-        if self._ws_sink:
-            self._ws_sink.set_property("access-key", creds.access_key_id)
-            self._ws_sink.set_property("secret-key", creds.secret_access_key)
-            self._ws_sink.set_property("session-token", creds.session_token)
+        if enabled:
+            if self._webrtc_task and not self._webrtc_task.done():
+                loguru.logger.warning("Streaming already enabled")
+                return
+
+            creds = await asyncio.to_thread(self.credential_provider.get_credentials)
+
+            self._kvs_client = self.kvs_client_factory(
+                region=self.aws_region,
+                credentials=creds,
+                video_track=self._gst_track,
+            )
+
+            self._webrtc_task = asyncio.create_task(self._kvs_client.run())
+            loguru.logger.info("Streaming started")
+
+        else:
+            if not self._webrtc_task or self._webrtc_task.done():
+                loguru.logger.warning("Streaming already disabled")
+                return
+
+            self._webrtc_task.cancel()
+            if self._kvs_client:
+                await self._kvs_client.stop()
+            self._kvs_client = None
+            loguru.logger.info("Streaming stopped")
 
     async def stop(self):
         self._running = False
 
+        await self.set_streaming_state(False)
+
+        if self._gst_track:
+            self._gst_track.stop()
+
         if self._task:
             await self._task
 
-        self._video_sink.disconnect(self._handler)
-        self._video_pipe.set_state(Gst.State.NULL)
+        if self._video_sink and self._handler:
+            self._video_sink.disconnect(self._handler)
+
+        if self._video_pipe:
+            self._video_pipe.set_state(Gst.State.NULL)
 
         self._video_pipe = None
         self._video_sink = None
-
-        self._frame_count = 0
-        self._last_process_time = 0
-        self._consecutive_detection_frames = 0
 
     def _decode_frame(self, sink):
         if not self._running:
             return Gst.FlowReturn.OK
 
         sample = sink.emit("pull-sample")
+        if not sample:
+            return Gst.FlowReturn.OK
+
         buf = sample.get_buffer()
         caps = sample.get_caps()
 
-        self._frame = np.ndarray(
-            (
-                caps.get_structure(0).get_value("height"),
-                caps.get_structure(0).get_value("width"),
-                3,
-            ),
-            buffer=buf.extract_dup(0, buf.get_size()),
-            dtype=np.uint8,
-        )
+        try:
+            h = caps.get_structure(0).get_value("height")
+            w = caps.get_structure(0).get_value("width")
+
+            array = np.ndarray(
+                (h, w, 3),
+                buffer=buf.extract_dup(0, buf.get_size()),
+                dtype=np.uint8,
+            )
+
+            self._frame = array
+            if self._gst_track:
+                self._gst_track.update_frame(array)
+
+        except Exception as e:
+            loguru.logger.warning(f"Frame decode error: {e}")
 
         return Gst.FlowReturn.OK
 
     async def _start_detection(self):
         while self._running:
-            if type(self._frame) == type(None):
+            if self._frame is None:
                 await asyncio.sleep(0.001)
                 continue
 
             current_time = time.time()
+            self._frame_count += 1
 
             if self._frame_count % self.sample_rate != 0:
                 await asyncio.sleep(0)
@@ -147,39 +189,36 @@ class StreamHandler:
                 await asyncio.sleep(0)
                 continue
 
-            detection, results = self._run_human_detection(self._frame)
+            detection, _ = self._run_human_detection(self._frame)
             if detection:
                 self._consecutive_detection_frames += 1
-
                 if (
                     self._consecutive_detection_frames
                     == self.presence_confirmation_frames
                 ):
                     self._send_detection_alert()
                     self._consecutive_detection_frames = 0
-                    continue
-
             else:
                 self._consecutive_detection_frames = 0
 
             self._last_process_time = current_time
             await asyncio.sleep(0)
 
-    def _run_human_detection(self, frame) -> Tuple[bool, list[AnyType] | None]:
-        results = self.model(frame, verbose=False)
-
-        for result in results:
-            boxes = result.boxes
-            for box in boxes:
-                class_id = int(box.cls[0])
-                confidence = float(box.conf[0])
-                # xyxy = box.xyxy[0].cpu().numpy()
-                # x1, y1, x2, y2 = map(int, xyxy)
-
-                if class_id == 0 and self.confidence_threshold <= confidence:
-                    return True, results
+    def _run_human_detection(self, frame) -> Tuple[bool, Optional[List[Any]]]:
+        try:
+            results = self.model(frame, verbose=False)
+            for result in results:
+                for box in result.boxes:
+                    cls = int(box.cls[0])
+                    conf = float(box.conf[0])
+                    if cls == 0 and conf >= self.confidence_threshold:
+                        return True, results
+        except Exception as e:
+            loguru.logger.error(f"Inference error: {e}")
 
         return False, None
 
     def _send_detection_alert(self):
-        loguru.logger.debug("[W.I.P] Placeholder detection alert!")
+        loguru.logger.warning("[W.I.P] Person Detected!")
+        # if self.mqtt_manager:
+        #     self.mqtt_manager.publish(self.alert_topic, {"event": "person_detected"})
